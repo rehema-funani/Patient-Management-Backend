eae53bf174fcce2a50264159b13952d0ae7e000a
makhatsa <brantonmakhatsa2@gmail.com>
2026-07-14 15:09:18 +0300

    adds files

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    INSTALLED_APPS = [
     "corsheaders",
-]
+
         "rest_framework",
 
     "accounts",
